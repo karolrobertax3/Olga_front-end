90fca58ad87b9463345e510c50d15dc8a039629c
unknown <helder825@hotmail.com>
2021-05-04 19:56:54 -0300

    rotas adicionadas

--- olga/src/app/app-routing.module.ts
+++ olga/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { ComprasComponent } from './compras/compras.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
+import { LojaComponent } from './loja/loja.component';
+import { ProdutosComponent } from './produtos/produtos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'inicio', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'cadastrar', component: CadastrarComponent},
+  {path: 'inicio', component: InicioComponent},
+  {path: 'produtos', component: ProdutosComponent},
+  {path: 'loja', component: LojaComponent},
+  {path: 'compras', component: ComprasComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
