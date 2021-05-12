2abf4b68f2151d4fb0ec66d078a494fc2655d696
unknown <helder825@hotmail.com>
2021-05-11 21:34:16 -0300

    metodo comprar consumido

--- olga/src/app/menu/menu.component.ts
+++ olga/src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-menu',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+
+  id = environment.idUsuario
+  foto = environment.foto
+  nome = environment.nome
 
   constructor() { }
 
