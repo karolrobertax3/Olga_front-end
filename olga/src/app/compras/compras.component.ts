659f40805fcd44a9664187a78f21258fd3077a46
unknown <helder825@hotmail.com>
2021-05-14 13:51:03 -0300

    atualizando componente compras

--- olga/src/app/compras/compras.component.ts
+++ olga/src/app/compras/compras.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
 import { Produtos } from '../model/Produtos';
 import { ProdutosComponent } from '../produtos/produtos.component';
+import { AlertasService } from '../service/alertas.service';
 import { UsuarioService } from '../service/usuario.service';
 
 interface itemCarrinho { 
@@ -37,12 +38,13 @@ export class ComprasComponent implements OnInit {
     private router: Router,
     private route: ActivatedRoute,
     private usuarioService : UsuarioService,
+    private alertas: AlertasService
   ) { }
 
   ngOnInit() {
     if(environment.token == ''){
       this.router.navigate(['/inicio'])
-      alert('Sua sessão expirou. Faça o login novamente!')
+      this.alertas.showAlertDanger('Sua sessão expirou. Faça o login novamente!')
     }
     this.findAllProdutos()
     console.log(this.idUser.toString())
@@ -51,7 +53,6 @@ export class ComprasComponent implements OnInit {
   findAllProdutos(){
     this.usuarioService.getAllProdutos().subscribe((resp: Produtos[]) =>{
       this.listaProdutos = resp
-      console.log(resp)
     })
   }
 
@@ -84,7 +85,7 @@ export class ComprasComponent implements OnInit {
 
     this.usuarioService.novoProduto(this.produto, this.idUser).subscribe((resp: Produtos)=>{
       this.produto = resp
-      alert('Produto cadastrado com sucesso!')
+      this.alertas.showAlertSuccess('Produto cadastrado com sucesso!')
       this.findAllProdutos()
       this.produto = new Produtos()
     })
@@ -142,12 +143,12 @@ export class ComprasComponent implements OnInit {
 //comprar itens carrinho
   comprar(){
     if(this.totalCompras.length <=0){
-      alert('O carrinho está vazio')
+      this.alertas.showAlertDanger('O carrinho está vazio')
       return;
     } else {
       this.totalCompras.map(item => {
         this.comprarProduto(item)
-        alert('Compra realizada com sucesso!')
+        this.alertas.showAlertSuccess('Compra realizada com sucesso!')
       })
     }
     this.findAllProdutos()
